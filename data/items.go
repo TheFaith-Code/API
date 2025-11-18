e0db1d8abcd5953c72b04535fc9e7ca05e212dcf
TheFaith-Code <94547838+TheFaith-Code@users.noreply.github.com>
2025-11-18 23:09:35 +0100

    added in-memory DB

---
+++ data/items.go
@@ -0,0 +1,10 @@
+package data
+
+import "go-backend-demo/models"
+
+// In-memory DB
+var Items = []models.Item{
+    {ID: 1, Name: "Alpha", Description: "First demo item"},
+    {ID: 2, Name: "Beta", Description: "Second demo item"},
+    {ID: 3, Name: "Gamma", Description: "Third demo item"},
+}
